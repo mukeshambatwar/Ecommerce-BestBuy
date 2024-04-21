b67ccc1d98781effa712c67ecd0f875a0607a756
Mukesh <mukesh.ambatwar92@gmail.com>
2024-04-21 16:43:30 +0530

    Store and Cart Functionality

--- store/models.py
+++ store/models.py
@@ -24,5 +24,35 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
     
+
+#VariationManager Class for managing the color and size of the product
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
     
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+
+
+#creating variation category choices
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+#product variation
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    #we are telling to Variation class that we have created a Variation Model is created 
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
+
 
