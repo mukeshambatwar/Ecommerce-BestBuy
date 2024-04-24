8d4c13ac7ce26404b21a0b71e44b79825f0bb204
Mukesh <mukesh.ambatwar92@gmail.com>
2024-04-24 15:50:46 +0530

    Django validations and Activations

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/',views.register, name='register'),
+    path('login/',views.login, name='login'),
+    path('logout/',views.logout, name='logout'),
+    path('dashboard/',views.dashboard, name='dashboard'),
+    path('',views.dashboard, name='dashboard'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
+
+#To activate this url templates.accounts.account_verification_email.html http://{{domain}}{% url 'activate' uidb64=uid token=token %} 
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+
+    #REset Password Email Activation URL
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+]
